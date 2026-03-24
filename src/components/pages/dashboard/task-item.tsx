a9f5e710f4cf0414b83518b40d5ca5e849c00ff9
Maha Abo Assi <eng.mahaab96@gmail.com>
2026-03-24 23:52:22 +0400

    fix is completed inside task item

--- src/components/pages/dashboard/task-item.tsx
+++ src/components/pages/dashboard/task-item.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Task } from "@/lib/mock-api"
 import { Clock } from "lucide-react"
 import { Checkbox } from "@/components/ui/checkbox"
@@ -7,17 +8,21 @@ import {
 } from "@/components/ui/field"
 import { Label } from "@/components/ui/label"
 import { Badge } from "@/components/ui/badge"
+import { useEffect, useState } from "react"
+
 type Props = {
     task: Task
 }
 
 const TaskItem = ({task}:Props)=>{
+    const [ isChecked, setIsChecked] = useState(false)
+    useEffect(()=>{setIsChecked(task.completed)},[task.completed])
     return(<li className="flex flex-col mobile-sm:flex-row mobile-md:flex-col desktop-sm:flex-row justify-between gap-2">
     <FieldGroup className="max-w-sm">
       <Field className="items-start" orientation="horizontal">
-        <Checkbox checked={task.completed} className="rounded-full" id="terms-checkbox" name={task.title} />
+        <Checkbox onCheckedChange={(val) => setIsChecked(!!val)} checked={isChecked} className="rounded-full" id="terms-checkbox" name={task.title} />
         <div className="flex flex-col gap-2">
-            <Label className={`${task.completed?"line-through":""} text-xs desktop-sm:text-base`} htmlFor="terms-checkbox">{task.title}</Label>
+            <Label className={`${isChecked?"line-through":""} text-xs desktop-sm:text-base`} htmlFor="terms-checkbox">{task.title}</Label>
             <div className="flex gap-2 items-center text-[0.7rem] desktop-sm:text-xs text-[var(--content-muted)] font-medium">
                 <Clock size={16} />
                 {task.dueLabel} &nbsp; task
