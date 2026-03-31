50bd6046edb062ec852e5a17ec8c01a6eed2af22
Maha Abo Assi <eng.mahaab96@gmail.com>
2026-03-31 07:32:40 +0400

    fix right and button for support icon

--- src/components/pages/dashboard/fixed-dashboard.tsx
+++ src/components/pages/dashboard/fixed-dashboard.tsx
@@ -7,7 +7,7 @@ const FixedInDashboard = ()=>{
     return(<div>
         <div className={theme === "dark" ? "blur-dark" : "blur-div"}>
         </div>
-        <div className={`fixed z-50 bottom-6 right-6 bg-[var(--content-emphasis)] text-[var(--content-inverted)] flex-center h-[32px] w-[32px] rounded-full cursor-pointer`}>
+        <div className={`fixed z-50 bottom-[26px] right-[21px] bg-[var(--content-emphasis)] text-[var(--content-inverted)] flex-center h-[32px] w-[32px] rounded-full cursor-pointer`}>
             <CircleQuestionMark className="size-[16px]" />
         </div>
     </div>)
